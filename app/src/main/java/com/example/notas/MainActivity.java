b6d669daf7bef531ad625fa74497f55040cdb81f
Miguel <migperales30@gmail.com>
2020-06-18 13:47:03 +0200

    sin persistencia

--- app/src/main/java/com/example/notas/MainActivity.java
+++ app/src/main/java/com/example/notas/MainActivity.java
@@ -3,31 +3,46 @@ package com.example.notas;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Note mTempNote=new Note();
+ private  NoteAdapter mNoteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnTemp=(Button)findViewById(R.id.btnTemporal);
-        btnTemp.setOnClickListener(new View.OnClickListener() {
+        mNoteAdapter=new NoteAdapter();
+        ListView listNote=(ListView)findViewById(R.id.list_view);
+        listNote.setAdapter(mNoteAdapter);
+
+        listNote.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                //Creamos una nueva intasncia de Dialog Show Note
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //Recupermos la nota de la posición pulsada por el usuario
+                Note tempNote=mNoteAdapter.getItem(position);
+                //creamos una instancia de show note
                 DialogShowNote dialog=new DialogShowNote();
-                //Indico al dialogo qué nota es la que debe mostrar por pantalla.
-                dialog.sendNoteSelected(mTempNote);
-                //Mostramos el dialoggo por pantalla con un manager
-                dialog.show(getSupportFragmentManager(),"note_show");
+                dialog.sendNoteSelected(tempNote);
+                dialog.show(getSupportFragmentManager(),"");
             }
         });
     }
@@ -42,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
     public void createNewNote(Note newNote){
 
         //Este método recibirá una nueva nota creada por el diálogo pertinente...
-        mTempNote=newNote;
+        mNoteAdapter.addNote(newNote);
     }
 
     @Override
@@ -53,5 +68,75 @@ public class MainActivity extends AppCompatActivity {
             dialog.show(getSupportFragmentManager(),"note_create");
         }
         return false;
+    }
+
+
+    public class NoteAdapter extends BaseAdapter{
+
+        List<Note> notelist= new ArrayList<>();
+
+        @Override
+        public int getCount() {
+            return notelist.size();
+        }
+
+        @Override
+        public Note getItem(int position) {
+            return notelist.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+
+            //Aquí va la lógica de las celdas de la lista
+
+            if (view==null){
+                /*en este if la vista no ha sido accedida anteriormente
+                asi que lo primero que hay que hacer es inflarla
+                a partir del layout lis_item.xml
+                 */
+
+                LayoutInflater inflater=(LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view =inflater.inflate(R.layout.list_item,viewGroup,false);
+
+            }
+
+            /*Vista bien definida y procedemos a ocultar las imagenes que sobren de layout...
+            1º cargamos los widgewts del ayout
+            * y rellenar titulo y descripción de la tarea
+            */
+            TextView texviewTitle =(TextView)view.findViewById(R.id.text_view_title);
+            TextView texviewDescription =(TextView)view.findViewById(R.id.text_view_description);
+
+            ImageView ivImportant = (ImageView)view.findViewById(R.id.image_vew_important);
+            ImageView ivTodo=(ImageView)view.findViewById(R.id.image_view_todo);
+            ImageView ivIdea=(ImageView)view.findViewById(R.id.image_view_idea);
+
+            Note currentNote=notelist.get(position);
+            if(!currentNote.isImportant()){
+                ivImportant.setVisibility(View.GONE);
+            }
+            if(!currentNote.isIdea()){
+                ivIdea.setVisibility(View.GONE);
+            }
+            if(!currentNote.isTodo()){
+                ivTodo.setVisibility(View.GONE);
+            }
+
+            texviewTitle.setText(currentNote.getTitle());
+            texviewDescription.setText(currentNote.getDescription());
+
+            return view;
+        }
+
+        public  void addNote(Note n){
+            notelist.add(n);
+            notifyDataSetChanged();
+        }
     }
 }
